6a1311b321153520c57547ec10ab433cf8e93f4b
Urinboev Abdushukur <abdushukururinboev@gmail.com>
2020-11-09 10:55:45 +0500

    This project is about todolist

---
+++ app.js
@@ -0,0 +1,49 @@
+let express = require("express");
+let bodyParser = require("body-parser");
+let date = require(__dirname + "/date.js")
+
+
+let app = express();
+let items = ["Buy Food", "Cook Food", "Eat Food"];
+let workItems = [];
+
+app.set('view engine', 'ejs');
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.get("/", function (req, res) {
+
+    let day = date.getDate();
+
+    res.render("list", {listTitle: day, newListItems: items});
+});
+
+app.post("/", function(req, res) {
+   let item = req.body.newItem;
+   if (req.body.list === "Work") {
+    workItems.push(item);
+    res.redirect("/work")
+} else
+items.push(item);
+res.redirect("/")
+   
+})
+
+app.get("/work", function(req, res) {
+    res.render("list", {listTitle: "Work List", newListItems: workItems});
+});  
+
+
+app.get("/about", function(req, res) {
+    res.render("about");
+})
+    
+
+
+app.listen(3000, function () {
+    console.log("Server is working");
+
+});
